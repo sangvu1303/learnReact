d7ae719ed8e018e781815d7012d42f7b62dc58dc
sang vu <vusang63@gmail.com>
2023-09-18 15:03:33 +0700

    commit example

---
+++ src/views/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+import MyComponent from './example/myComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello world with React.js </p>
+        
+        <MyComponent/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/example/ChildComponent.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import './Demo.scss'
+
+//class components - statefull components
+class ChildComponent extends React.Component {
+
+    state = {
+        showJobs:false
+    }
+
+    handleShowHide = () => {
+        this.setState({
+            showJobs: !this.state.showJobs
+        })
+    }
+
+    handleOnclickDelete = (job) => {
+        console.log('>>> handleOnclickDelete: ', job)
+        this.props.deleteAJob(job)
+    }
+
+    render() {
+        
+        let { arrJobs } = this.props;
+        let { showJobs } = this.state;
+        let check = showJobs === true ? 'showJobs = true' : 'showJobs = false';
+        console.log('>>> check conditional: ', check)
+
+        return (
+            <>
+                {showJobs === false ? 
+                    <div>
+                        <button className='btn-show'
+                        onClick={()=> this.handleShowHide()}>Show</button>
+                    </div> 
+                    :  
+                    <>
+                        <div className="job-lists">
+                        {
+                            arrJobs.map((item, index) => {
+                                return (
+                                    <div key={item.id}>
+                                        {item.title} - {item.salary}$ 
+                                        <></> <span onClick={() => this.handleOnclickDelete(item)}> x </span>
+                                    </div>
+                                )
+                            })
+                        }
+                        </div>
+                        <div>
+                            <button onClick={()=> this.handleShowHide()}>Hide</button>
+                        </div>
+                    </>
+                }               
+            </>
+        )
+    }
+}
+
+
+
+// arrow function components - stateless components - use with hook
+// const ChildComponent = (props)=>{
+        
+//         let { name, age, address, arrJobs } = props;
+
+//         return (
+//             <>
+//                 <div>name: {name} - age: {age} - address: {address}</div>
+//                 <div className="job-lists">
+//                     {
+//                         arrJobs.map((item, index) => {
+//                             return (
+//                                 <div key={item.id}>
+//                                     {item.title} - {item.salary}
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
+//             </>
+//         )
+// }
+
+export default ChildComponent;
