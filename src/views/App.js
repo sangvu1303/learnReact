04d70f53eb56bdec6ae4a4f9890fe18704d1e530
sang vu <vusang63@gmail.com>
2023-09-22 15:00:57 +0700

    commit Axios & route

--- src/views/App.js
+++ src/views/App.js
@@ -6,11 +6,13 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Nav from './Nav/Nav';
 import Home from './Example/Home';
+import ListUser from './Users/ListUser';
 import {
   BrowserRouter,
   Switch,
   Route
 } from "react-router-dom";
+import DetailUser from './Users/DetailUser';
 
 function App() {
   return (
@@ -28,6 +30,12 @@ function App() {
             </Route>
             <Route path="/about">
               <MyComponent />
+            </Route>
+            <Route path="/user" exact>
+              <ListUser/>
+            </Route>
+            <Route path="/user/:id">
+              <DetailUser/>
             </Route>
           </Switch>
 
